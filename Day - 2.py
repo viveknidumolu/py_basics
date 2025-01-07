90ab660740465589a9e10752e718807d22e892ca
Vivek Nidumolu <viveknidumolu@gmail.com>
2025-01-07 19:33:32 +0530

    Add files via upload

---
+++ Day - 2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Create a List, tuple and Dictionary with 5 elements in it and how to access few elements based on the index. Try  with different examples 
+
+# In[1]:
+
+
+# Creation
+my_list = [10, 20, 30, 40, 50]  
+my_tuple = ("apple", "banana", "cherry", "orange", "mango")  
+my_dict = {"name": "Alice", "age": 25, "city": "New York", "job": "Engineer", "hobby": "Reading"}  
+
+print("Element at index 0:", my_list[0])
+print("Element at index 3:", my_list[3])
+print("Last element:", my_list[-1])
+
+print("Element at index 1:", my_tuple[1])
+print("Element at index 3:", my_tuple[-2])
+print("First element:", my_tuple[0])
+
+print("Value for key 'name':", my_dict["name"])
+print("Value for key 'city':", my_dict["age"])
+print("Value for key 'hobby':", my_dict["hobby"])
+
+
+# In[ ]:
+
+
+
+
