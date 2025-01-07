cd0a0f24d76899fba28f40a35489f1dfefec0624
Vivek Nidumolu <viveknidumolu@gmail.com>
2025-01-07 19:23:41 +0530

    Add files via upload

---
+++ Day - 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. Jupyter Notebook and Data Types
+# Write a program in Jupyter Notebook to declare variables of different data types (integer, float, string, and boolean). Print each variable and its type.
+# 
+
+# In[1]:
+
+
+a = 10          
+b = 3.14          
+c = "Hello, World!" 
+d = True        
+print("Integer Value:", a, " | Type:", type(a))
+print("Float Value:", b, " | Type:", type(b))
+print("String:", c, " | Type:", type(c))
+print("Boolean Value:", d, " | Type:", type(d))
+
+
+# In[ ]:
+
+
+
+
